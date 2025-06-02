757f9a743d5833e3faf5be19dce15a01040e9fad
Ben Muthalaly <benmuthalaly@gmail.com>
2025-06-02 13:59:26 -0500

    Display image.

--- src/main.zig
+++ src/main.zig
@@ -2,18 +2,20 @@ const std = @import("std");
 const rl = @import("raylib");
 
 pub fn main() !void {
-    rl.initWindow(800, 600, "hello world!");
+    rl.initWindow(800, 1000, "hello world!");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
 
+    const file_name: [:0]const u8 = "test-images/img.jpeg";
+    const image = try rl.Image.init(file_name);
+    const image_texture = try rl.loadTextureFromImage(image);
+
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
-        rl.clearBackground(rl.Color.blue);
-    }
 
-    const file_name: [:0]const u8 = "test-images/img.jpeg";
-    const image = try rl.Image.init(file_name);
-    std.debug.print("valid: {}\n", .{rl.isImageValid(image)});
+        rl.clearBackground(rl.Color.blue);
+        rl.drawTextureEx(image_texture, .{ .x = 0, .y = 0 }, 0, 0.35, rl.Color.ray_white);
+    }
 }
